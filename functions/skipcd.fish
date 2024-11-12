28ec5a659fbe44b80fb4e2e5f13cbcdd1e8b0ba6
Tatsuro Nakamura <me@tatsuroro.com>
2024-11-12 19:07:28 +0900

    Add support for multiple directories

--- functions/skipcd.fish
+++ functions/skipcd.fish
@@ -3,12 +3,20 @@ function skipcd
     builtin cd $argv
     or return
 
-    if set -q SKIPCD_REPO_PATH
-        # check if the current directory is in the specified repository
-        set -l toplevel (git rev-parse --show-toplevel 2>/dev/null)
-        if [ "$toplevel" = "$SKIPCD_REPO_PATH" ]
-            if set -q SKIPCD_SUBDIR_NAME; and [ -d $SKIPCD_SUBDIR_NAME ]
-                builtin cd $SKIPCD_SUBDIR_NAME
+    # 環境変数をカンマで分割してリストにする
+    set SKIPCD_REPO_PATHS (string split ',' $SKIPCD_REPO_PATH)
+    set SKIPCD_SUBDIR_NAMES (string split ',' $SKIPCD_SUBDIR_NAME)
+
+    # 現在のディレクトリが指定されたリポジトリのいずれかにあるか確認
+    set -l toplevel (git rev-parse --show-toplevel 2>/dev/null)
+
+    for repo_path in $SKIPCD_REPO_PATHS
+        if [ "$toplevel" = "$repo_path" ]
+            for subdir_name in $SKIPCD_SUBDIR_NAMES
+                if [ -d $subdir_name ]
+                    builtin cd $subdir_name
+                    return
+                end
             end
         end
     end
